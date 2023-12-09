5b49f9b6d82b5132b296f35f7b219ae88be4feba
Pascual Miranda <pascualmn@gmail.com>
2023-12-09 13:51:36 -0400

    Primer commit del sicoem

---
+++ src/main/java/com/compubol/sicoem/SicoemApplication.java
@@ -0,0 +1,14 @@
+package com.compubol.sicoem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SicoemApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hola mundo");
+		SpringApplication.run(SicoemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/compubol/sicoem/SicoemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compubol.sicoem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SicoemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
