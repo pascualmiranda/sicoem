81b96c4d8b593e49766883b3430b0e5cc7f219a6
Pascual Miranda <pascualmn@gmail.com>
2024-04-07 20:52:04 -0400

    Ajustes default de WebSecurityConfig

--- src/main/java/com/compubol/sicoem/security/WebSecurityConfig.java
+++ src/main/java/com/compubol/sicoem/security/WebSecurityConfig.java
@@ -1,7 +1,9 @@
 package com.compubol.sicoem.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -10,8 +12,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig {
 
     @Bean
@@ -20,26 +25,8 @@ public class WebSecurityConfig {
                 .authorizeHttpRequests((requests) -> requests
                         .requestMatchers("/**","/swagger-ui/**", "/swagger-ui/index.html").permitAll()
                         .anyRequest().authenticated()
-                        //.anyRequest().permitAll()
                 )
-                .formLogin((form) -> form
-                        .loginPage("/login")
-                        .permitAll()
-                )
-                .logout((logout) -> logout.permitAll());
-
+                .formLogin(withDefaults());
         return http.build();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("password")
-                        .roles("USER")
-                        .build();
-
-        return new InMemoryUserDetailsManager(user);
     }
 }
