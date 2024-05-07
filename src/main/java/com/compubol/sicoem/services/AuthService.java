fb07a8e78ec276dd8a87fa9d2e6aa113fb20cf10
Pascual Miranda <pascualmn@gmail.com>
2024-05-06 22:38:03 -0400

    Login autentification con Spring security

---
+++ src/main/java/com/compubol/sicoem/services/AuthService.java
@@ -0,0 +1,27 @@
+package com.compubol.sicoem.services;
+import com.compubol.sicoem.domain.dto.AuthResponse;
+import com.compubol.sicoem.domain.dto.LoginRequest;
+import com.compubol.sicoem.repositories.UsuarioRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final UsuarioRepository userRepository;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthResponse login(LoginRequest request) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
+        UserDetails user=userRepository.findByLogin(request.getUsername()).orElseThrow();
+        String token=jwtService.getToken(user);
+        return AuthResponse.builder()
+                .token(token)
+                .build();
+
+    }
+}
